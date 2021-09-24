d21236d945e64cf96ac398a9374b36583edef68e
MayNghii <91300528+MayNghii@users.noreply.github.com>
2021-09-24 10:09:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introTitle: 'Anh ơiii!'
+    introDesc: `Tui có này muốn hỏi..bạn nhớ phải trả lời thật lòng nhơ =))`,
+    btnIntro: 'Dạ anh nghe',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
